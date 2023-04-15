3dd345e4250d5679366ccea364ecde31ea0f7c1b
Aniket <aniketsthorat11@gmail.com>
2023-04-15 23:44:44 +0530

    changed App.js

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>App is Running </h1>
+        <h1>App is Running in my pc</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
